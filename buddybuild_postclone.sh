6056670128d0f299ba836c8b44bbc064bf55a8fa
Dave Kerr <dwmkerr@gmail.com>
2017-03-01 22:56:40 +0800

    Added script to limit buddybuild to just the third app

---
+++ buddybuild_postclone.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# BuddyBuild will get confused if it sees all of these apps! So start
+# by removing all the example apps except '3_native_app'.
+rm -rf 1_react_native_app/
+rm -rf 2_ionic_app/
