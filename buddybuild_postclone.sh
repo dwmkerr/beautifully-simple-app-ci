e8097d9a2a4c61bc12ed5254bf00d0107ba072f0
Dave Kerr <dwmkerr@gmail.com>
2017-04-03 18:00:24 +0800

    Typeo fixed

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -19,7 +19,7 @@ fi
 # Install Node 6.
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"i
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 6
 nvm use 6
 
