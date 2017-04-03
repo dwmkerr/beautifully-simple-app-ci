4b8cf20100476351620863d0cb6a389a57354133
Dave Kerr <dwmkerr@gmail.com>
2017-04-03 17:37:42 +0800

    Another try with travis and buddybuild

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -6,16 +6,29 @@ rm -rf 1_react_native_app/
 rm -rf 2_ionic_app/
 rm -rf 4_xamarinapp/
 
+# Install ImageMagick on OSX.
+if [ "$(uname)" == "Darwin" ]; then
+	# OSX - install HomeBrew and then ImageMagick.
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew install imagemagick
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	# OSX - install ImageMagick.
+	apt-get install imagemagick
+fi
+
+# Install Node 6.
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+nvm install 6
+nvm use 6
+
 # Try some more installation approaches.
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install imagemagick
 
 # Install the dependencies.
 # brew install imagemagick
-curl https://www.imagemagick.org/download/binaries/ImageMagick-x86_64-apple-darwin16.4.0.tar.gz | tar xvz
-export MAGICK_HOME="$PWD/ImageMagick-x86_64-apple-darwin16.4.0"
-export PATH="$MAGICK_HOME/bin:$PATH"
-export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib/"
+# curl https://www.imagemagick.org/download/binaries/ImageMagick-x86_64-apple-darwin16.4.0.tar.gz | tar xvz
+# export MAGICK_HOME="$PWD/ImageMagick-x86_64-apple-darwin16.4.0"
+# export PATH="$MAGICK_HOME/bin:$PATH"
+# export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib/"
 
 
 # Label the app.
